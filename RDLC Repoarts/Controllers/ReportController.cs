23ef4de4de3a58a5a426ed51df09c87f4a0079de
M N Tushar <mntushar25@gmail.com>
2022-04-07 16:31:54 +0600

    Report controlar

--- RDLC Repoarts/Controllers/ReportController.cs
+++ RDLC Repoarts/Controllers/ReportController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RDLC_Repoarts.Controllers
+namespace RDLCRepoarts.Controllers
 {
     public class ReportController : Controller
     {
+        private IWebHostEnvironment _webHostEnvironment;
+
+        public ReportController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,7 +25,15 @@ namespace RDLC_Repoarts.Controllers
 
         public IActionResult Print()
         {
-            return View();
+            string mimtype = "";
+            int extention = 1;
+            var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\Report1.rdlc";
+            Dictionary<string, string> parametrs = new Dictionary<string, string>();
+            parametrs.Add("rp1", "Product RDLC report");
+            LocalReport localReport = new LocalReport(path);
+            var result = localReport.Execute(RenderType.Pdf, extention, parametrs, mimtype);
+
+            return File(result.MainStream, "application/pdf");
         }
     }
 }
