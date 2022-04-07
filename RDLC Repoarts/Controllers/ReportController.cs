eefb0ce6d1cb2fcb0821f4ed1725887663ddbe6d
M N Tushar <mntushar25@gmail.com>
2022-04-07 15:27:46 +0600

    report designe

---
+++ RDLC Repoarts/Controllers/ReportController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RDLC_Repoarts.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Print()
+        {
+            return View();
+        }
+    }
+}
